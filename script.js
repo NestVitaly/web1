8d5d2bee55697c257054f3617a3db8bb2a307e7c
NVitaly <whiskalik@gmail.com>
2022-12-19 19:10:40 +0300

    Сайт с рассказом

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = prompt('Введите свое имя: ');
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("name1").innerHTML = name;
+        document.getElementById("name2").innerHTML = name;
+        var date = prompt('Введите дату своего рождения: ', 'dd-mm-yyyy');
+        document.getElementById("date").innerHTML = date;
+        var hobbi = prompt('Введите свое хобби: ');
+        document.getElementById("hobbi").innerHTML = hobbi;
